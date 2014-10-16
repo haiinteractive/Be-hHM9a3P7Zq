00752a039cd6a2893b326f191286435e77a22202
vijaymjay <vijaym.jay@gmail.com>
2014-10-17 00:03:41 +0530

    change js file for validation

--- assets/app/js/beclouds.js
+++ assets/app/js/beclouds.js
@@ -1,5 +1,5 @@
 
-//var base_url = 'http://localhost/vijay/codeigniter/';
+// var base_url = 'http://localhost/vijay/git_beclouds/';
 
 var base_url = '/media';
 
@@ -30,8 +30,7 @@ var base_url = '/media';
                   call( params, request_url );
               });
 			  
-			  
-				$("#loginbtn").on('click', function(){
+			  $("#loginbtn").on('click', function(){
 				
 					var firstname = $("#us_first_name").val();
 					var lastname = $("#us_last_name").val();
@@ -39,44 +38,12 @@ var base_url = '/media';
 					var password = $("#uspwd").val();
 					var user_roles = $("#us_roles").val();
 					
-					if(firstname == "")
-					{
-						alert('Please enter First Name');
-						$( "#us_first_name" ).focus();
-					}
-					else if(lastname == "")
-					{
-						alert('Please enter Last Name');
-						$( "#us_last_name" ).focus();
-					}
-					else if(email=="")
-					{
-						alert('Please enter Email');
-						$( "#us_email" ).focus();
-					}
-					else if(validateEmail(email)==false)
-					{
-						alert('Please enter valid email address');
-						$( "#us_email" ).focus();
-					}
-					else if(password == "")
-					{
-						alert('Please enter password');
-						$( "#uspwd" ).focus();
-					}
-					else if(user_roles == "")
-					{
-						alert('Please select user roles');
-						$( "#us_roles" ).focus();
-					}
-					else
+					if(firstname != "" && lastname != "" && email !=""  && password != "" && user_roles != "" && validateEmail(email)==true)
 					{
 						 var params = 'user_first_name='+$("#us_first_name").val()+'&user_last_name='+$("#us_last_name").val()+'&email='+$("#us_email").val()+'&userpwd='+$("#uspwd").val()+'&user_role='+$("#us_roles").val()+'&purpose=employee';
                   var request_url = base_url+'/employee/add';
                   		call( params, request_url );	
 					}
-					
-					
 					
 					 function validateEmail(email) {
 						var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
@@ -88,8 +55,8 @@ var base_url = '/media';
 							return false;
 						}
 					}
-					
 				});
+			  
 				
 				$("#edit_loginbtn").on('click', function(){
 				
@@ -99,36 +66,13 @@ var base_url = '/media';
 					var password = $("#usepwd").val();
 					var user_roles = $("#use_roles").val();
 					
-					
-					if(firstname == "")
-					{
-						alert('Please enter First Name');
-						$( "#use_first_name" ).focus();
-					}
-					else if(lastname == "")
-					{
-						alert('Please enter Last Name');
-						$( "#use_last_name" ).focus();
-					}
-					
-					else if(validateEmail(email)==false)
-					{
-						alert('Please enter valid email address');
-						$( "#use_email" ).focus();
-					}
-					
-					else if(user_roles == "")
-					{
-						alert('Please select user roles');
-						$( "#use_roles" ).focus();
-					}
-					else
+					if(firstname != "" && lastname != "" && email !=""  && password != "" && user_roles != "" && validateEmail(email)==true)
 					{
 						 var params = 'user_first_name='+$("#use_first_name").val()+'&user_last_name='+$("#use_last_name").val()+'&email='+$("#use_email").val()+'&userpwd='+$("#usepwd").val()+'&user_role='+$("#use_roles").val()+'&use_status='+$("#use_status").val()+'&usereid='+$("#usereid").val()+'&purpose=employee';
                   var request_url = base_url+'/employee/update';
                   		call( params, request_url );	
 					}
-				
+					
 					 function validateEmail(email) {
 						var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 		
@@ -140,41 +84,42 @@ var base_url = '/media';
 						}
 					}
 					
+				
+				});
+				
+				
+				$('form').submit(function(event){
+
+				  event.preventDefault();
+				
 				});
 				
 				
 				$("#categorybtn").on('click', function(){
-														 
+							
 						var cat_name = $("#cat_name").val();
 						
-						if(cat_name == "")
-						{
-							alert('Please enter Category Name');
-							$( "#cat_name" ).focus();
-						}
-						else
+						if(cat_name != "")
 						{
 							 var params = 'cat_name='+$("#cat_name").val()+'&purpose=category';
+							 
 					  var request_url = base_url+'/category/add';
-							call( params, request_url );	
+							call( params, request_url );
 						}
+						
 				});
 				
 				$("#edit_categorybtn").on('click', function(){
 														 
 						var cat_name = $("#cate_name").val();
 						
-						if(cat_name == "")
-						{
-							alert('Please enter Category Name');
-							$( "#cate_name" ).focus();
-						}
-						else
+						if(cat_name != "")
 						{
 							 var params = 'cat_name='+$("#cate_name").val()+'&cat_status='+$("#cat_status").val()+'&cat_id='+$("#cate_id").val()+'&purpose=category';
 					  var request_url = base_url+'/category/update';
-							call( params, request_url );	
+							call( params, request_url );
 						}
+						
 				});
 				
 				
@@ -182,34 +127,27 @@ var base_url = '/media';
 														 
 						var product_name = $("#product_name").val();
 						
-						if(product_name == "")
-						{
-							alert('Please enter Product Name');
-							$( "#product_name" ).focus();
-						}
-						else
+						if(product_name != "")
 						{
 							 var params = 'product_name='+$("#product_name").val()+'&purpose=product';
-					  var request_url = base_url+'/product/add';
+							  var request_url = base_url+'/product/add';
 							call( params, request_url );	
 						}
+						
 				});
 				
 				$("#edit_productbtn").on('click', function(){
 														 
 						var proe_name = $("#proe_name").val();
 						
-						if(proe_name == "")
-						{
-							alert('Please enter Product Name');
-							$( "#proe_name" ).focus();
-						}
-						else
+						if(proe_name != "")
 						{
 							 var params = 'product_name='+$("#proe_name").val()+'&product_status='+$("#product_status").val()+'&product_id='+$("#product_id").val()+'&purpose=product';
 					  var request_url = base_url+'/product/update';
-							call( params, request_url );	
+							call( params, request_url );
+							
 						}
+						
 				});
 				
 				
@@ -220,24 +158,7 @@ var base_url = '/media';
 					var ad_type_code = $("#ad_type_code").val();
 					var ad_type_rate = $("#ad_type_rate").val();
 					
-					
-					if(ad_type_name == "")
-					{
-						alert('Please enter Name');
-						$( "#ad_type_name" ).focus();
-					}
-					else if(ad_type_code=="")
-					{
-						alert('Please enter code');
-						$( "#ad_type_code" ).focus();
-					}
-					
-					else if(ad_type_rate == "")
-					{
-						alert('Please enter Rack Rate');
-						$( "#ad_type_rate" ).focus();
-					}
-					else
+					if(ad_type_name != "" && ad_type_code != "" && ad_type_rate !="")
 					{
 						 var params = 'form_adtype='+$("#form_adtype").val()+'&ad_type_name='+$("#ad_type_name").val()+'&ad_type_code='+$("#ad_type_code").val()+'&ad_type_rate='+$("#ad_type_rate").val()+'&purpose=adtype';
                   var request_url = base_url+'/adtype/add';
@@ -253,23 +174,8 @@ var base_url = '/media';
 					var ad_type_code = $("#edit_type_code").val();
 					var ad_type_rate = $("#edit_rack_rate").val();
 					
-					if(ad_type_name == "")
-					{
-						alert('Please enter Name');
-						$( "#edit_type_name" ).focus();
-					}
-					else if(ad_type_code=="")
-					{
-						alert('Please enter code');
-						$( "#edit_type_code" ).focus();
-					}
 					
-					else if(ad_type_rate == "")
-					{
-						alert('Please enter Rack Rate');
-						$( "#edit_rack_rate" ).focus();
-					}
-					else
+					if(ad_type_name != "" && ad_type_code != "" && ad_type_rate !="")
 					{
 						 var params = 'form_adtype='+$("#edit_type_roles").val()+'&ad_type_name='+$("#edit_type_name").val()+'&ad_type_code='+$("#edit_type_code").val()+'&ad_type_rate='+$("#edit_rack_rate").val()+'&status='+$("#edtype_status").val()+'&adtype_id='+$("#adtype_id").val()+'&purpose=adtype';
                   var request_url = base_url+'/adtype/update';
@@ -290,51 +196,12 @@ var base_url = '/media';
 					var email = $("#email").val();
 					var alt_email = $("#alt_email").val();
 					
-					
-					if(c_name == "")
-					{
-						alert('Please enter Company Name');
-						$( "#c_name" ).focus();
-					}
-					else if(c_person=="")
-					{
-						alert('Please enter Contact Person');
-						$( "#c_person" ).focus();
-					}
-					
-					else if(address == "")
-					{
-						alert('Please enter Address');
-						$( "#address" ).focus();
-					}
-					else if(pin_code == "")
-					{
-						alert('Please enter Pincode');
-						$( "#pin_code" ).focus();
-					}
-					else if(phone == "")
-					{
-						alert('Please enter Phone');
-						$( "#phone" ).focus();
-					}
-					else if(email == "")
-					{
-						alert('Please enter Email');
-						$( "#email" ).focus();
-					}
-					else if(validateEmail(email)==false)
-					{
-						alert('Please enter valid email address');
-						$( "#email" ).focus();
-					}
-					
-					else
+					if(c_name != "" && c_person != "" && address !="" && pin_code !="" && phone !="" && email !="" && validateEmail(email)==true)
 					{
 						 var params = 'c_name='+$("#c_name").val()+'&c_person='+$("#c_person").val()+'&address='+$("#address").val()+'&pin_code='+$("#pin_code").val()+'&phone='+$("#phone").val()+'&alt_phone='+$("#alt_phone").val()+'&email='+$("#email").val()+'&alt_email='+$("#alt_email").val()+'&purpose=company';
                   var request_url = base_url+'/company/add';
                   		call( params, request_url );	
 					}
-					
 					
 					 function validateEmail(email) {
 						var filter = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
@@ -346,6 +213,7 @@ var base_url = '/media';
 							return false;
 						}
 					}
+					
 				});
 				
 				
@@ -358,44 +226,7 @@ var base_url = '/media';
 					var phone = $("#cphone").val();
 					var email = $("#cemail").val();
 					
-					if(c_name == "")
-					{
-						alert('Please enter Company Name');
-						$( "#co_name" ).focus();
-					}
-					else if(c_person=="")
-					{
-						alert('Please enter Contact Person');
-						$( "#co_person" ).focus();
-					}
-					
-					else if(address == "")
-					{
-						alert('Please enter Address');
-						$( "#caddress" ).focus();
-					}
-					else if(pin_code == "")
-					{
-						alert('Please enter Pincode');
-						$( "#cpin_code" ).focus();
-					}
-					else if(phone == "")
-					{
-						alert('Please enter Phone');
-						$( "#cphone" ).focus();
-					}
-					else if(email == "")
-					{
-						alert('Please enter Email');
-						$( "#cemail" ).focus();
-					}
-					else if(validateEmail(email)==false)
-					{
-						alert('Please enter valid email address');
-						$( "#cemail" ).focus();
-					}
-					
-					else
+					if(c_name != "" && c_person != "" && address !="" && pin_code !="" && phone !="" && email !="" && validateEmail(email)==true)
 					{
 						 var params = 'c_name='+$("#co_name").val()+'&c_person='+$("#co_person").val()+'&address='+$("#caddress").val()+'&pin_code='+$("#cpin_code").val()+'&phone='+$("#cphone").val()+'&email='+$("#cemail").val()+'&comp_id='+$("#comp_id").val()+'&status='+$("#comp_status").val()+'&purpose=company';
                   var request_url = base_url+'/company/update';
